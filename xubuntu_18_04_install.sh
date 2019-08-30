8312b4dc27b59708915f7bb8417de1abf79c47e4
Ivan <ivanpfalcao@hotmail.com>
2019-08-30 17:53:44 -0300

    ma prepare  and dbeaver

--- xubuntu_18_04_install.sh
+++ xubuntu_18_04_install.sh
@@ -1,61 +1,70 @@
-sudo apt update
+apt update
 
-sudo apt install -y dpkg
+apt install -y dpkg
+apt install -y module-assistant
+m-a prepare
+
 # Linguagens
-sudo apt install -y build-essential
-sudo apt install -y openjdk-8-jdk
-sudo apt install -y python3-dev
-sudo apt install -y python3-pip
+apt install -y build-essential
+apt install -y openjdk-8-jdk
+apt install -y python3-dev
+apt install -y python3-pip
 
 # gedit
-sudo apt install -y gedit
+apt install -y gedit
 
 # Browsers
-sudo apt install -y chromium-browser
+apt install -y chromium-browser
 
 # Scala
-sudo apt install -y scala
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get update
-sudo apt-get install -y sbt
+apt install -y scala
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+apt-get update
+apt-get install -y sbt
 
 # VSCodium
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
-echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list
-sudo apt update
-sudo apt install codium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | apt-key add -
+echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | tee --append /etc/apt/sources.list
+apt update
+apt install codium
 
 # Docker
-sudo apt install -y docker
-sudo apt install -y docker-compose
+apt install -y docker
+apt install -y docker-compose
 
 # Spark
 cd /opt
-sudo wget https://archive.apache.org/dist/spark/spark-2.2.1/spark-2.2.1-bin-hadoop2.7.tgz
-sudo tar -xvf spark-2.2.1-bin-hadoop2.7.tgz
-sudo rm spark-2.2.1-bin-hadoop2.7.tgz
-sudo chmod 777 -R spark-2.2.1-bin-hadoop2.7
+wget https://archive.apache.org/dist/spark/spark-2.2.1/spark-2.2.1-bin-hadoop2.7.tgz
+tar -xvf spark-2.2.1-bin-hadoop2.7.tgz
+rm spark-2.2.1-bin-hadoop2.7.tgz
+chmod 777 -R spark-2.2.1-bin-hadoop2.7
 
 # Zeppelin
 cd /opt
-sudo wget https://archive.apache.org/dist/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz
-sudo mv zeppelin-0.8.1-bin-all.tgz /opt
+wget https://archive.apache.org/dist/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz
+mv zeppelin-0.8.1-bin-all.tgz /opt
 cd /opt
-sudo tar -xvf zeppelin-0.8.1-bin-all.tgz
+tar -xvf zeppelin-0.8.1-bin-all.tgz
 rm zeppelin-0.8.1-bin-all.tgz
-sudo chmod 777 -R zeppelin-0.8.1-bin-all
+chmod 777 -R zeppelin-0.8.1-bin-all
 
 # POSTMAN
 cd /opt
-sudo wget https://dl.pstmn.io/download/latest/linux64
-sudo tar -xvf linux64
-sudo rm linux64
-sudo chmod 777 -R Postman
+wget https://dl.pstmn.io/download/latest/linux64
+tar -xvf linux64
+rm linux64
+chmod 777 -R Postman
 
 #ROBO3t
 cd /opt
-sudo wget https://download-test.robomongo.org/linux/robo3t-1.3.1-linux-x86_64-7419c406.tar.gz
-sudo tar -xvf robo3t-1.3.1-linux-x86_64-7419c406.tar.gz
-sudo rm robo3t-1.3.1-linux-x86_64-7419c406.tar.gz
-sudo chmod 777 -R robo3t-1.3.1-linux-x86_64-7419c406
+wget https://download-test.robomongo.org/linux/robo3t-1.3.1-linux-x86_64-7419c406.tar.gz
+tar -xvf robo3t-1.3.1-linux-x86_64-7419c406.tar.gz
+rm robo3t-1.3.1-linux-x86_64-7419c406.tar.gz
+chmod 777 -R robo3t-1.3.1-linux-x86_64-7419c406
+
+
+cd /tmp
+wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
+dpkg -i dbeaver-ce_latest_amd64.deb
+
