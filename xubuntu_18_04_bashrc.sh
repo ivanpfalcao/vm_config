d733fc441e6bdd5b49b243d44e1c0497217612cf
Ivan <ivanpfalcao@hotmail.com>
2020-05-30 17:59:02 +0000

    Update xubuntu_18_04_bashrc.sh

--- xubuntu_18_04_bashrc.sh
+++ xubuntu_18_04_bashrc.sh
@@ -16,5 +16,5 @@ export PATH=$PATH:$SPARK_HOME/bin
 #export PATH=$PATH:$ZOOKEEPER_HOME/bin
 
 # ZEPPELIN
-export ZEPPELIN_HOME=/opt/zeppelin-0.8.2-bin-all
-export PATH=$PATH:$ZEPPELIN_HOME/bin
+#export ZEPPELIN_HOME=/opt/zeppelin-0.8.2-bin-all
+#export PATH=$PATH:$ZEPPELIN_HOME/bin
