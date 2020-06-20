c86dd0e309e0fad793ddd9ad773252d2f195b5c5
Ivan <ivanpfalcao@hotmail.com>
2020-06-20 14:45:31 +0000

    Update xubuntu_18_04_install.sh

--- xubuntu_18_04_install.sh
+++ xubuntu_18_04_install.sh
@@ -132,6 +132,15 @@ install_vscodium() {
     apt install -y codium
 }
 
+install_vscode() {
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+    sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    apt-get install apt-transport-https
+    apt-get update
+    apt-get install code    
+}
+
 
 
 
@@ -222,7 +231,8 @@ install_microk8s() {
 
 ubuntu_install
 configure_docker
-install_vscodium
+#install_vscodium
+install_vscode
 install_scala
 install_spark
 install_postman
